64c68609ea2de61988b4d5dfb428e1527f23fa29
samratpro <samratpro25@gmail.com>
2022-04-27 02:16:50 +0600

    Complete Weather by Scrapping

--- weather/app/tests.py
+++ weather/app/tests.py
@@ -1,3 +1,26 @@
+from tempfile import SpooledTemporaryFile
+from attr import attrs
 from django.test import TestCase
 
 # Create your tests here.
+from bs4 import BeautifulSoup as bs
+import requests
+
+
+
+def weather(city):
+    url = f'https://www.google.com/search?q={city}+weather&gl=us&hl=en'
+    headers = {"user-agen":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.3",
+              "referer":"https://fonts.googleapis.com/",
+              "origin":"https://surferseo.com",
+              }
+    response = requests.get(url, headers=headers)
+    soup = bs(response.text, 'html.parser')
+    result = {}
+    result['city_name'] = soup.find_all('span', {'class':'BNeawe tAd8D AP7Wnd'})[0].text
+    result['data'] = soup.find_all('div', {'class':'BNeawe tAd8D AP7Wnd'})[1].text.split(sep='\n')[0]
+    result['temp'] = soup.find_all('div', {'class':'BNeawe iBp4i AP7Wnd'})[1].text
+    return result
+
+
+print(weather('khulna'))
--- weather/app/views.py
+++ weather/app/views.py
@@ -3,8 +3,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from bs4 import BeautifulSoup as bs
+import requests
+
+def weather(city):
+    url = f'https://www.google.com/search?q={city}+weather&gl=us&hl=en'
+    headers = {"user-agen":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.3",
+              "referer":"https://fonts.googleapis.com/",
+              "origin":"https://surferseo.com",
+              }
+    response = requests.get(url, headers=headers)
+    soup = bs(response.text, 'html.parser')
+    result = {}
+    try:
+        result['city_name'] = soup.find_all('span', {'class':'BNeawe tAd8D AP7Wnd'})[0].text
+        result['data'] = soup.find_all('div', {'class':'BNeawe tAd8D AP7Wnd'})[1].text.split(sep='\n')[0]
+        result['temp'] = soup.find_all('div', {'class':'BNeawe iBp4i AP7Wnd'})[1].text
+    except:
+        result['error'] = ''
+    return result
+
 
 def home(request):
-    template = 'home.html'
+    template = 'weather/home.html'
+    if request.method == 'GET' and 'city_search' in request.GET:
+        city = request.GET.get('city_search')
+        result = weather(city)
+        if 'error' in result:
+            contex = {'error':result}
+        else:
+            contex = {'result':result}
+    else:
+        contex = {}
 
-    return render(request, template)
+    return render(request, template, contex)
